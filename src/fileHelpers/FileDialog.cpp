b9d7902ad09a94df18609639007cc267766ba5a0
Sebastian Heerwald <sebastian.heerwald@st.ovgu.de>
2017-10-29 19:52:50 +0100

    #1 Added file dialog for linux

--- src/fileHelpers/FileDialog.cpp
+++ src/fileHelpers/FileDialog.cpp
@@ -1,11 +1,21 @@
 #include "FileDialog.h"
 
-#include <windows.h>
-#include <shlobj.h>
+#ifdef SYSTEM_WINDOWS
+  #include <windows.h>
+  #include <shlobj.h>
+#endif
+#ifdef SYSTEM_LINUX
+  #include <gtk/gtk.h>
+  #include <cstring>
+  #include <malloc.h>
+#endif
+
+#include <iostream>
 #include <cassert>
 
 namespace FileDialog
 {
+#ifdef SYSTEM_WINDOWS
     // Function that opens a dialoge window to choose a DICOM file
     std::string openFilename( char *filter = "*.dcm", HWND owner = NULL )
     {
@@ -75,4 +85,230 @@ namespace FileDialog
             return false;
         }
     }
+#endif
+
+#ifdef SYSTEM_LINUX
+    static void WaitForCleanup(void)
+    {
+        while (gtk_events_pending())
+            gtk_main_iteration();
+    }
+
+    static void AddTypeToFilterName( const char *typebuf, char *filterName, size_t bufsize )
+    {
+        const char SEP[] = ", ";
+
+        size_t len = strlen(filterName);
+        if ( len != 0 )
+        {
+            strncat( filterName, SEP, bufsize - len - 1 );
+            len += strlen(SEP);
+        }
+
+        strncat( filterName, typebuf, bufsize - len - 1 );
+    }
+
+    static bool NFDi_IsFilterSegmentChar( char ch )
+    {
+        return (ch==','||ch==';'||ch=='\0');
+    }
+
+    static void AddFiltersToDialog( GtkWidget *dialog, const char *filterList )
+    {
+        GtkFileFilter *filter;
+        char typebuf[4096] = {0};
+        const char *p_filterList = filterList;
+        char *p_typebuf = typebuf;
+        char filterName[4096] = {0};
+
+        if ( !filterList || strlen(filterList) == 0 )
+            return;
+
+        filter = gtk_file_filter_new();
+        while ( 1 )
+        {
+
+            if ( NFDi_IsFilterSegmentChar(*p_filterList) )
+            {
+                char typebufWildcard[4096];
+                /* add another type to the filter */
+                assert( strlen(typebuf) > 0 );
+                assert( strlen(typebuf) < 4096-1 );
+
+                snprintf( typebufWildcard, 4096, "*.%s", typebuf );
+                AddTypeToFilterName( typebuf, filterName, 4096 );
+
+                gtk_file_filter_add_pattern( filter, typebufWildcard );
+
+                p_typebuf = typebuf;
+                memset( typebuf, 0, sizeof(char) * 4096 );
+            }
+
+            if ( *p_filterList == ';' || *p_filterList == '\0' )
+            {
+                /* end of filter -- add it to the dialog */
+
+                gtk_file_filter_set_name( filter, filterName );
+                gtk_file_chooser_add_filter( GTK_FILE_CHOOSER(dialog), filter );
+
+                filterName[0] = '\0';
+
+                if ( *p_filterList == '\0' )
+                    break;
+
+                filter = gtk_file_filter_new();
+            }
+
+            if ( !NFDi_IsFilterSegmentChar( *p_filterList ) )
+            {
+                *p_typebuf = *p_filterList;
+                p_typebuf++;
+            }
+
+            p_filterList++;
+        }
+
+        /* always append a wildcard option to the end*/
+
+        filter = gtk_file_filter_new();
+        gtk_file_filter_set_name( filter, "*.*" );
+        gtk_file_filter_add_pattern( filter, "*" );
+        gtk_file_chooser_add_filter( GTK_FILE_CHOOSER(dialog), filter );
+    }
+
+    static void SetDefaultPath( GtkWidget *dialog, const char *defaultPath )
+    {
+        if ( !defaultPath || strlen(defaultPath) == 0 )
+            return;
+
+        /* GTK+ manual recommends not specifically setting the default path.
+           We do it anyway in order to be consistent across platforms.
+           If consistency with the native OS is preferred, this is the line
+           to comment out. -ml */
+        gtk_file_chooser_set_current_folder( GTK_FILE_CHOOSER(dialog), defaultPath );
+    }
+
+    bool openFilename( std::string filterList,
+                       std::string defaultPath,
+                       std::string & outPath )
+    {
+        GtkWidget *dialog;
+        bool result;
+
+        if ( !gtk_init_check( NULL, NULL ) )
+        {
+            return false;
+        }
+
+        dialog = gtk_file_chooser_dialog_new( "Open File",
+                                              NULL,
+                                              GTK_FILE_CHOOSER_ACTION_OPEN,
+                                              "_Cancel", GTK_RESPONSE_CANCEL,
+                                              "_Open", GTK_RESPONSE_ACCEPT,
+                                              NULL );
+
+        /* Build the filter list */
+        AddFiltersToDialog(dialog, filterList.c_str() );
+
+        /* Set the default path */
+        SetDefaultPath(dialog, defaultPath.c_str() );
+
+        result = false;
+        if ( gtk_dialog_run( GTK_DIALOG(dialog) ) == GTK_RESPONSE_ACCEPT )
+        {
+            char *filename;
+
+            filename = gtk_file_chooser_get_filename( GTK_FILE_CHOOSER(dialog) );
+
+            {
+                outPath = std::string( filename );
+                if ( outPath.empty() )
+                {
+                    g_free( filename );
+                    gtk_widget_destroy(dialog);
+                    return false;
+                }
+            }
+            g_free( filename );
+
+            result = true;
+        }
+
+        WaitForCleanup();
+        gtk_widget_destroy(dialog);
+        WaitForCleanup();
+
+        return result;
+    }
+
+    bool openFolder( std::string defaultPath,
+                     std::string& outPath )
+    {
+        GtkWidget *dialog;
+        bool result;
+
+        if (!gtk_init_check(NULL, NULL))
+        {
+            return false;
+        }
+
+        dialog = gtk_file_chooser_dialog_new( "Select folder",
+                                              NULL,
+                                              GTK_FILE_CHOOSER_ACTION_SELECT_FOLDER,
+                                              "_Cancel", GTK_RESPONSE_CANCEL,
+                                              "_Select", GTK_RESPONSE_ACCEPT,
+                                              NULL );
+        gtk_file_chooser_set_do_overwrite_confirmation( GTK_FILE_CHOOSER(dialog), TRUE );
+
+
+        /* Set the default path */
+        SetDefaultPath( dialog, defaultPath.c_str() );
+
+        result = false;
+        if ( gtk_dialog_run( GTK_DIALOG(dialog) ) == GTK_RESPONSE_ACCEPT )
+        {
+            char *filename = gtk_file_chooser_get_filename( GTK_FILE_CHOOSER(dialog) );
+
+            {
+                outPath = std::string( filename );
+                if ( outPath.empty() )
+                {
+                    g_free( filename );
+                    gtk_widget_destroy(dialog);
+                    return false;
+                }
+            }
+            g_free(filename);
+
+            result = true;
+        }
+
+        WaitForCleanup();
+        gtk_widget_destroy(dialog);
+        WaitForCleanup();
+
+        return result;
+    }
+
+    bool openDialog( std::string const& rootDir,
+                     FileDialogType type,
+                     std::string filter,
+                     std::string & file )
+    {
+        if( type == FDTFolder )
+        {
+            return openFolder( rootDir, file );
+        }
+        else if( type == FDTFile )
+        {
+            return openFilename( filter, rootDir, file );
+        }
+        else
+        {
+            assert( false && "unknown dialog type" );
+            std::cerr << "unknown dialog type" << std::endl;
+            return false;
+        }
+    }
+#endif
 } // namespace FileDialog
