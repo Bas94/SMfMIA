3f4eed4f3833f82913c791626d4a0e126f641411
Sebastian Heerwald <sebastian.heerwald@st.ovgu.de>
2017-10-29 21:47:40 +0100

    #1 some documentation

--- src/fileHelpers/DICOMLoaderVTK.h
+++ src/fileHelpers/DICOMLoaderVTK.h
@@ -12,8 +12,18 @@
 
 namespace DICOMLoaderVTK
 {
+    /*!
+     * \brief loadDICOM loads a single 2D DICOM slice
+     * \param filename is a path to the DICOM file which we want to load
+     * \return returns a 2D image data filled with the 2D DICOM slice
+     */
 	vtkSmartPointer<vtkImageData> loadDICOM(std::string filename);
 
-	vtkSmartPointer<vtkImageData> loadDICOMSeries(std::string directory);
+    /*!
+     * \brief loadDICOMSeries loads a DICOM series out of a given directory
+     * \param directory is the directory where the DICOM series is loaded from
+     * \return returns 3D image data filled with the DICOM series
+     */
+    vtkSmartPointer<vtkImageData> loadDICOMSeries( std::string directory );
 };
 
