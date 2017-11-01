4b9bc851a4a0428ef329bbf635b70fb838cef402
Sebastian Heerwald <sebastian.heerwald@st.ovgu.de>
2017-11-01 02:40:09 +0100

    #1 cleanup of includes and some minor things

--- src/fileHelpers/DICOMLoaderVTK.cpp
+++ src/fileHelpers/DICOMLoaderVTK.cpp
@@ -1,9 +1,16 @@
 #include "DICOMLoaderVTK.h"
 
+#include <vtkImageData.h>
+#include <vtkImageViewer2.h>
+#include <vtkDICOMImageReader.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+
 namespace DICOMLoaderVTK
 {
 	// Function that read in a single DICOM Image
-	vtkSmartPointer<vtkImageData> loadDICOM(std::string filename) 
+    vtkSmartPointer<vtkImageData> loadDICOM( std::string filename )
 	{	
 		vtkSmartPointer<vtkDICOMImageReader> reader =
 			vtkSmartPointer<vtkDICOMImageReader>::New();
@@ -16,14 +23,14 @@ namespace DICOMLoaderVTK
 	}
 
 	// Function that read in DICOM Series
-	vtkSmartPointer<vtkImageData> loadDICOMSeries(std::string directory)
+    vtkSmartPointer<vtkImageData> loadDICOMSeries( std::string directory )
 	{
 		// Read all the DICOM files in the specified directory.
 		vtkSmartPointer<vtkDICOMImageReader> reader =
 			vtkSmartPointer<vtkDICOMImageReader>::New();
 		reader->SetDirectoryName(directory.c_str());
 		
-		// TODO: check if directory contains DICOM Images, but don´t know how...
+		// TODO: check if directory contains DICOM Images, but donï¿½t know how...
 		
 		reader->Update();
 
@@ -31,4 +38,5 @@ namespace DICOMLoaderVTK
 
 		return imageData;		
 	}
-};
+
+} // namespace DICOMLoaderVTK
--- src/fileHelpers/DICOMLoaderVTK.h
+++ src/fileHelpers/DICOMLoaderVTK.h
@@ -1,12 +1,8 @@
 #pragma once
 #include <string>
-#include <vtkImageData.h>
 #include <vtkSmartPointer.h>
-#include <vtkImageViewer2.h>
-#include <vtkDICOMImageReader.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkRenderer.h>
+
+class vtkImageData;
 
 namespace DICOMLoaderVTK
 {
@@ -15,7 +11,7 @@ namespace DICOMLoaderVTK
      * \param filename is a path to the DICOM file which we want to load
      * \return returns a 2D image data filled with the 2D DICOM slice
      */
-	vtkSmartPointer<vtkImageData> loadDICOM(std::string filename);
+    vtkSmartPointer<vtkImageData> loadDICOM( std::string filename );
 
     /*!
      * \brief loadDICOMSeries loads a DICOM series out of a given directory
