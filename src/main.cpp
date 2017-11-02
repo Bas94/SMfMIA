d349d2efcbe43f39e628616e5aefc19a4b4d552c
LifY05 <lif.aka.midna@googlemail.com>
2017-11-02 14:54:42 +0100

    uninteressante Änderung die verwurfen werden soll

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@
 int main( int argc, char** argv )	
 {
 	// TODO: global path??
-	std::string inputFilename = "c:\\DatenE\\02WiSe1718\\03SMMIA\\Projekt\\data\\p01\\0_pre\\00_data\\export0001.dcm";
-	std::string directory = "c:\\DatenE\\02WiSe1718\\03SMMIA\\Projekt\\data\\p01\\0_pre\\00_data";
+	std::string inputFilename = "c:\\Users\\lifak\\Documents\\Studium\\09_Master_Wi1718\\SM-MIA\\data\\p01\\0_pre\\00_data\\export0001.dcm";
+	std::string directory = "c:\\Users\\lifak\\Documents\\Studium\\09_Master_Wi1718\\SM-MIA\\data\\p01\\0_pre\\00_data";
 	
 	DICOMLoaderVTK::loadDICOM(inputFilename);
 
