109c38491351e666b5b77ffef593520246bc8b3a
Sebastian Heerwald <sebastian.heerwald@st.ovgu.de>
2017-11-30 00:46:47 +0100

    #8 Added converting function for converting contour to itk image

--- src/ContourToMask.cpp
+++ src/ContourToMask.cpp
@@ -18,7 +18,41 @@ cv::Mat3b compute( Contour const & contour , int *dim )
     cv::fillPoly( mask, fillContAll, cv::Scalar( 255, 255, 255 ) );
 
     return mask;
-//    return maskData;
+    //    return maskData;
+}
+
+MaskType2D::Pointer computeITK( Contour const & contour, int *dim )
+{
+    cv::Mat3b mask = compute( contour, dim );
+
+    MaskType2D::RegionType region;
+    MaskType2D::IndexType start;
+    start[0] = 0;
+    start[1] = 0;
+
+    MaskType2D::SizeType size;
+    size[0] = dim[0];
+    size[1] = dim[1];
+
+    region.SetSize(size);
+    region.SetIndex(start);
+
+    MaskType2D::Pointer image = MaskType2D::New();
+    image->SetRegions(region);
+    image->Allocate();
+
+    MaskType2D::IndexType index;
+    for( int y = 0; y < dim[1]; ++y )
+    {
+        index[1] = y;
+        for( int x = 0; x < dim[0]; ++x )
+        {
+            index[0] = x;
+            image->SetPixel( index, mask( y, x )[0] > 0 );
+        }
+    }
+
+    return image;
 }
 
 } // namespace ContourToMask
