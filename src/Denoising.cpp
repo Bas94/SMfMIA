32a1eb59193d8ca90a84f774d2e5481f64fc6af7
elke94 <elke@grabe-online.de>
2017-12-20 23:20:36 +0100

    add a second bilateralfilter function for 2d images

--- src/Denoising.cpp
+++ src/Denoising.cpp
@@ -26,4 +26,6 @@ namespace Denoising
 
         return Converter::ConvertITKToVTK<ImageType>(image);
 	}
+
+	
 }
--- src/Denoising.h
+++ src/Denoising.h
@@ -3,6 +3,20 @@
 #include <vtkImageData.h>
 
 namespace Denoising 
-{
+{	
 	vtkSmartPointer<vtkImageData> bilateralFilter(vtkSmartPointer<vtkImageData> imageData, double domainSigma, double rangeSigma);
+
+	template<typename itkImageType>
+	typename itkImageType::Pointer bilateralFilter2D(typename itkImageType::Pointer itkImagePointer, double domainSigma, double rangeSigma)
+	{
+		typedef itk::BilateralImageFilter<itkImageType, itkImageType >	BilateralImageFilter2D;
+		BilateralImageFilter2D::Pointer bilateralFilter = BilateralImageFilter2D::New();
+		bilateralFilter->SetInput(itkImagePointer);
+		bilateralFilter->SetDomainSigma(domainSigma);
+		bilateralFilter->SetRangeSigma(rangeSigma);
+		bilateralFilter->Update();
+
+		itkImageType::Pointer outputImage = bilateralFilter->GetOutput();
+		return outputImage;
+	}
 }
