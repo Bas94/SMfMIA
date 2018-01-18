63e67a648af8faa40530b24f610cc438e33e15b9
elke94 <elke@grabe-online.de>
2018-01-18 07:37:52 +0100

    added a function to write DCM files

---
+++ src/fileHelpers/WriteDCM.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <itkImage.h>
+#include <string.h>
+#include "itkImageSeriesWriter.h"
+
+#include "Helpers/TypeDefinitions.h"
+
+
+	template<typename itkImageType>
+	void writeDCMSeries(typename itkImageType::Pointer itkImage, std::string _filename, std::string directory)
+	{
+		typedef itk::ImageSeriesWriter< itkImageType, ImageType2D>  WriterType;
+
+		std::vector<std::string> filenames;
+
+		for (int i = 1; i <= itkImage->GetLargestPossibleRegion().GetSize()[2]; i++)
+		{
+			std::string filename = directory;
+			filename += _filename;
+			filename += std::to_string(i);
+			filename += ".dcm";
+
+			filenames.push_back(filename);
+		}
+
+		WriterType::Pointer writer = WriterType::New();
+		writer->SetFileNames(filenames);
+		//writer1->SetFileName("C:\\DatenE\\02WiSe1718\\03SMMIA\\Projekt\\data\\p01\\1_int\\outputBiasCorrection\\test.dcm");
+		writer->SetInput(itkImage);
+		writer->Update();
+	}
